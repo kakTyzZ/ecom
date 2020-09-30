42af32a03eaa53b48a63b4e918c3d15f10ed63e4
kaktyzz <kaktyzz-imba@mail.ru>
2020-09-30 23:58:38 +0300

    ecom11

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,10 +1,30 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
 for(var i=0; i < updateBtns.length; i++){
-    updateBtns[i].addEventListener('click', function(){
-    var productId = this.dataset.product
-    var action = this.dataset.action
-    console.log('productId:', productId, 'action:',action)
+            updateBtns[i].addEventListener('click', function(){
+            var productId = this.dataset.product
+            var action = this.dataset.action
+            console.log('productId:', productId, 'action:',action)
 
+            console.log('USER:',user)
+            if(user == "AnonymousUser"){
+                console.log('Not logged in')
+            }else{
+                updateUserOrder(productId,action)
+            }
        })
+}
+
+function updateUserOrder(productId, action){
+    console.log('User is logged in, sending data..')
+
+    var url = 'update_item/'
+
+    fetch(url, {
+        method:'POST',
+        header:{
+            'Content-Type':'application/json'
+        },
+
+    })
 }
